420cc794933dad57c75a74da5815567413456c37
karapetyan <karapetyan@inftest.ru>
2020-11-24 15:52:33 +0300

    IT-19 Реализовал прототипы

--- vectormodel.cpp
+++ vectormodel.cpp
@@ -1,24 +1,111 @@
 #include "vectormodel.h"
 
-VectorModel::VectorModel(QString nameTester, QObject *parent) : SubtableModel(parent)
-{
-    /*
-     * Устанавливает связь с тестером, может читает статус тестера и тд
-    */
-    Q_UNUSED(nameTester);
+#include <QQmlEngine>
+
+static bool registerMe() {
+    qmlRegisterType<VectorModel>(VectorModel::MODULE_NAME.toUtf8(), 12, 34, VectorModel::ITEM_NAME.toUtf8());
+    return true;
 }
 
-QVariant VectorModel::data(const QModelIndex &index, int role) const
-{
+const QString VectorModel::MODULE_NAME = "VectorM";
+const QString VectorModel::ITEM_NAME = "VectorModel";
+const bool VectorModel::IS_QML_REG = registerMe();
+
+const std::array<QString, VectorModel::VectorRoleCOUNT> VectorModel::VECTOR_ROLE_STR {
+    // cell
+    "ReserveData",
+    // vertical header
+    "AddBreakPoint"
+};
+
+VectorModel::VectorModel(QObject *parent) : SubtableModel(parent) {
+}
+
+bool VectorModel::setConfigPin(QByteArray data) {
+    Q_UNUSED(data);
+    return false;
+}
+
+bool VectorModel::setVectors(QByteArray data) {
+    Q_UNUSED(data);
+    return false;
+}
+
+bool VectorModel::setErrorMap(QByteArray data) {
+    Q_UNUSED(data);
+    return false;
+}
+
+QPoint VectorModel::currentIndex() const {
+    return QPoint(-1, -1);
+}
+
+bool VectorModel::setCurrentIndex(QPoint p) {
+    Q_UNUSED(p);
+    return false;
+}
+
+QRect VectorModel::selection() const {
+    return QRect(-1, -1, 0, 0);
+}
+
+bool VectorModel::setSelection(QRect sel) {
+    Q_UNUSED(sel);
+    return false;
+}
+
+bool VectorModel::setSelectionAndCurrentIndex(QRect sel, QPoint p) {
+    Q_UNUSED(sel);
+    Q_UNUSED(p);
+    return false;
+}
+
+bool VectorModel::tactView(bool isTact) {
+    Q_UNUSED(isTact);
+    return false;
+}
+
+bool VectorModel::prevError() {
+    return false;
+}
+
+bool VectorModel::nextError() {
+    return false;
+}
+
+bool VectorModel::prevFind(QString data, bool inBus) {
+    Q_UNUSED(data);
+    Q_UNUSED(inBus);
+    return false;
+}
+
+bool VectorModel::nextFind(QString data, bool inBus) {
+    Q_UNUSED(data);
+    Q_UNUSED(inBus);
+    return false;
+}
+
+QStringListModel* VectorModel::pins() {
+    return new QStringListModel(this);
+}
+
+QVariant VectorModel::data(const QModelIndex &index, int role) const {
+    Q_UNUSED(index);
+    Q_UNUSED(role);
     return QVariant();
 }
 
-int VectorModel::totalRowCount() const
-{
+bool VectorModel::setData(const QModelIndex &index, const QVariant &value, int role) {
+    Q_UNUSED(index);
+    Q_UNUSED(value);
+    Q_UNUSED(role);
+    return false;
+}
+
+int VectorModel::totalRowCount() const {
     return 0;
 }
 
-int VectorModel::totalColumnCount() const
-{
+int VectorModel::totalColumnCount() const {
     return 0;
 }
--- vectormodel.h
+++ vectormodel.h
@@ -2,30 +2,163 @@
 
 #include "subtablemodel.h"
 
+#include <QPoint>
+#include <QRect>
+#include <QStringListModel>
+
 class VectorModel : public SubtableModel
 {
     Q_OBJECT
+    Q_PROPERTY(QPoint currentIndex READ currentIndex WRITE setCurrentIndex NOTIFY currentIndexChanged)
+    Q_PROPERTY(QRect selection READ selection WRITE setSelection NOTIFY selectionChanged)
 public:
     static const QString MODULE_NAME;   // VectorM
     static const QString ITEM_NAME;     // VectorModel
     static const bool IS_QML_REG;
-    VectorModel(QString nameTester, QObject *parent = nullptr);
-    virtual ~VectorModel() = default;
 
     enum VectorRole {
         // cell
-        VectorRoleReserveData,
+        VectorRoleReserveData,      // Для тестирования.
         // vertical header
-        VectorRoleAddBreakPoint,
+        VectorRoleAddBreakPoint,    // Для возможности установки breakpoint.
 
         VectorRoleCOUNT
     };
     Q_ENUM(VectorRole)
     static const std::array<QString, VectorRoleCOUNT> VECTOR_ROLE_STR;
 
+    VectorModel(QObject *parent = nullptr);
+    virtual ~VectorModel() = default;
+
+    /*!
+     * \brief Соотвествие пинов и название столбцов
+     * \param data - данные конфигурации.
+     * \return true, если успешно.
+     */
+    bool setConfigPin(QByteArray data);
+
+    /*!
+     * \brief Получение данных векторов
+     * \param data - данные векторов.
+     * \return true, если успешно.
+     */
+    bool setVectors(QByteArray data);
+
+    /*!
+     * \brief Получает карту ошибок
+     *
+     * Так же получает данные результатов запуска и др.
+     * \param data - данные карты ошибок
+     * \return true, если успешно.
+     */
+    bool setErrorMap(QByteArray data);
+
+    /*!
+     * \brief Получение активной ячейки
+     *
+     * x - столбцы, y - строки.
+     * \return активную ячейку.
+     */
+    QPoint currentIndex() const;
+
+    /*!
+     * \brief Установка активной ячейки
+     *
+     * x - столбцы, y - строки.
+     * \param p - активная ячейка.
+     * \return true, если успех.
+     */
+    bool setCurrentIndex(QPoint p);
+
+    /*!
+     * \brief Выделенная область
+     *
+     * x,y указывают начало выделение, а width/height на направление и
+     * размер.
+     * Если width == 0 или height == 0, выделена один столбец или одна строка.
+     * Если width/height имеют положительное значение, направление выделение
+     * вправо/вниз. Если отрицательные, то влево/вверх.
+     * \return Выделенную область.
+     */
+    QRect selection() const;
+
+    /*!
+     * \brief Установка выделенной области
+     * \param sel - область.
+     * \return true, если успех.
+     */
+    bool setSelection(QRect sel);
+
+    /*!
+     * \brief Установка области и ячейки
+     * \param sel - выделенная область.
+     * \param p - активная ячейка
+     * \return true, если успех.
+     */
+    bool setSelectionAndCurrentIndex(QRect sel, QPoint p);
+
+    /*!
+     * \brief Показывает столбец тактов
+     *
+     * Сортирует вектора по тактам. Активно только после получения карты
+     * ошибок.
+     * \param isTact - true, для показа тактов, иначе без тактов.
+     * \return true, если успешно.
+     */
+    Q_INVOKABLE bool tactView(bool isTact);
+
+    /*!
+     * \brief Перемещает активную ячейку к предыдущей ошибке
+     *
+     * Работает по кругу.
+     * \return false, когда нет ошибок.
+     */
+    Q_INVOKABLE bool prevError();
+
+    /*!
+     * \brief Перемещает активную ячейку к следующей ошибке
+     *
+     * Работает по кругу.
+     * \return false, когда нет ошибок.
+     */
+    Q_INVOKABLE bool nextError();
+
+    /*!
+     * \brief Поиск в направлении начала
+     *
+     * Работает по кругу.
+     * \param data - строка поиска.
+     * \param inBus - если false, поиск по каналам. Если true, поиск по шинам.
+     * \return false, когда нет результатов поиска
+     */
+    Q_INVOKABLE bool prevFind(QString data, bool inBus);
+
+    /*!
+     * \brief Поиск в направлении конца
+     *
+     * Работает по кругу.
+     * \param data - строка поиска.
+     * \param inBus - если false, поиск по каналам. Если true, поиск по шинам.
+     * \return false, когда нет результатов поиска
+     */
+    Q_INVOKABLE bool nextFind(QString data, bool inBus);
+
+    /*!
+     * \brief Модель заголовков
+     *
+     * Будет менятся в зависимости от конфигураций пинов и при сворачивании
+     * или разворачивании шин.
+     * \return модель заголовков.
+     */
+    Q_INVOKABLE QStringListModel* pins();
+
+signals:
+    void currentIndexChanged();
+    void selectionChanged();
 public:
-    QVariant data(const QModelIndex &index, int role) const override;
-    int totalRowCount() const override;
-    int totalColumnCount() const override;
+    Q_INVOKABLE QVariant data(const QModelIndex &index, int role) const override;
+    Q_INVOKABLE bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    Q_INVOKABLE int totalRowCount() const override;
+    Q_INVOKABLE int totalColumnCount() const override;
 };
 
